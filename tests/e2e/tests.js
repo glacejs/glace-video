ac7282c3103db4c5f47e4889887e0e98d945779a
Sergei Chipiga <chipiga86@gmail.com>
2018-04-02 19:01:13 +0300

    Fix wrong file path

--- tests/e2e/tests.js
+++ tests/e2e/tests.js
@@ -2,12 +2,14 @@
 
 test("Record video of display", null, [fxVideo], () => {
     chunk(async () => {
+        console.log("video will be saved to", SS.getVideo({ check: false }));
         await SS.pause(5, "just pause");
     });
 });
 
 test("Record video of browser", null, [fxKillWebdriver, fxSelenium, fxWebdriver, fxBrowser, fxVideo], () => {
     chunk(async () => {
+        console.log("video will be saved to", SS.getVideo({ check: false }));
         await SS.pause(5, "just pause");
     });
 });
