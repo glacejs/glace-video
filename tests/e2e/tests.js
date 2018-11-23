fcfd0ffb2f018e6676c3f8caccf59211d3c9131d
Sergei Chipiga <sergei.chipiga@pipedrive.com>
2018-11-23 13:41:32 +0200

    Use fixtures factory

--- tests/e2e/tests.js
+++ tests/e2e/tests.js
@@ -1,14 +1,14 @@
 "use strict";
 
 suite("e2e tests", () => {
-    test("Record video of display", null, [fxVideo], () => {
+    test("Record video of display", [fxVideo], () => {
         chunk(async () => {
             console.log("video will be saved to", $.getVideo({ check: false }));
             await $.pause(5, "just pause");
         });
     });
     
-    test("Record video of browser", null, [fxKillWebdriver, fxSelenium, fxBrowser, fxVideo], () => {
+    test("Record video of browser", [fxKillWebdriver, fxSeleniumServer, fxBrowser, fxVideo], () => {
         chunk(async () => {
             console.log("video will be saved to", $.getVideo({ check: false }));
             await $.pause(5, "just pause");
